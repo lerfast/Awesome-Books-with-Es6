9c88f66ddab31b716f9d353514e1a8251bb6935a
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-07-10 13:09:55 -0500

    Update dates.js
    Doing code corrections

--- modules/dates.js
+++ modules/dates.js
@@ -1,19 +1,3 @@
-// import { DateTime } from './luxon.js';
-
-// export function displayCurrentDate() {
-//   const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
-//   const dateContainer = document.getElementById('date');
-//   dateContainer.innerHTML = `<small>${currentDate}</small>`;
-// }
-// import { DateTime } from './luxon.js';
-
-// export function displayCurrentDate() {
-//   const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
-//   const dateContainer = document.getElementById('date');
-//   dateContainer.innerHTML = `<small>${currentDate}</small>`;
-// }
-
-// dates.js
 import { DateTime } from './luxon.js';
 
 export default function displayCurrentDate() {
